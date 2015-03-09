a114bb08dfdfac1603c5b6dfa005f929765c98f7
dari <darius.rueckert@fau.de>
2015-03-09 11:28:01 +0100

    object3d changes

--- libhello/rendering/object3d.h
+++ libhello/rendering/object3d.h
@@ -17,13 +17,16 @@ public:
     //TODO: extra class so uniform objects are faster
     glm::quat rot;
 //    mat4 rotation;
-    mat4 size;
-    mat4 base;
+//    mat4 size;
+    vec3 scale = vec3(1);
+    vec3 position = vec3(0);
+//    mat4 base;
 
-    void getModelMatrix(mat4& m){m = base*glm::mat4_cast(rot)*size;}
-    void calculateModel(){model = base*glm::mat4_cast(rot)*size;}
+    mat4 getModelMatrix();
+    void getModelMatrix(mat4& model);
+    void calculateModel();
 
-   vec4 getPosition(){return vec4(base[3]);} //returns global position
+   vec4 getPosition(){return vec4(position,1);} //returns global position
     vec4 getDirection(){return rot*vec4(0,0,1,0);} //returns looking direction
     vec4 getRightVector(){return rot*vec4(1,0,0,0);}
     vec4 getUpVector(){return rot*vec4(0,1,0,0);}
@@ -39,15 +42,15 @@ public:
     void rotateGlobal(vec3 axis, float angle);
 
 
-    void scale(vec3 s);
+//    void scale(vec3 s);
     vec3 getScale();
     void setScale(vec3 s);
     void normalize();
 
-    virtual void setPosition(vec3 cords){base[3] = vec4(cords,1);}
+    virtual void setPosition(vec3 cords){position = cords;}
     virtual void turn(float angleX, float angleY);
 
-    virtual void getViewMatrix(mat4& view); //the view matrix is the inverse model matrix
+//    virtual void getViewMatrix(mat4& view); //the view matrix is the inverse model matrix
 
 
 };
