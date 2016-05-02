08adac975aa234512bcbc0862fc80cb38029872e
dari <darius.rueckert@fau.de>
2016-05-03 00:07:43 +0200

    shader api documentation and more debug asserts

--- saiga/opengl/shader/shader.h
+++ saiga/opengl/shader/shader.h
@@ -15,122 +15,170 @@ class raw_Texture;
  */
 
 class SAIGA_GLOBAL Shader{
+private:
+	/**
+	* The program id of the currently bound shader. In debug and testing mode all shader functions,
+	* that require the shader to be bound check this id with the own id.
+	*
+	* Additionally unbind has to be called for every bind. A new shader bind, when an
+	* other shader is already bound will result in an error in debug and testing mode.
+	*/
+	static GLuint boundShader;
 public:
+	static GLuint getBoundShader(){ return boundShader; }
 
-    GLuint program = 0;
-    std::vector<std::shared_ptr<ShaderPart>> shaders;
-    std::string name;
-
-
-    Shader();
-    virtual ~Shader();
-    Shader(Shader const&) = delete;
-    Shader& operator=(Shader const&) = delete;
+public:
+	GLuint program = 0;
+	std::vector<std::shared_ptr<ShaderPart>> shaders;
+	std::string name;
 
 
-    // ===================================== program stuff =====================================
-
-    void bind();
-    void unbind();
-    GLuint createProgram();
-    void destroyProgram();
-    void printProgramLog();
-
-    // ===================================== Compute Shaders =====================================
-    // Note: Compute shaders require OpenGL 4.3+
-
-    /**
-     * Returns the work group size specified in the shader.
-     *
-     * For example:
-     * layout(local_size_x = 32, local_size_y = 32) in;
-     *
-     * returns glm::uvec3(32,32,1)
-     */
-
-    glm::uvec3 getComputeWorkGroupSize();
-
-    /**
-     * Calculates the number of groups required for the given problem size.
-     * The number of shader executions will then be greater or equal to the problem size.
-     */
-
-    glm::uvec3 getNumGroupsCeil( const glm::uvec3 &problem_size);
-    glm::uvec3 getNumGroupsCeil( const glm::uvec3 &problem_size,const glm::uvec3 &work_group_size);
-
-    /**
-     * Initates the compute operation.
-     * The shader must be bound beforehand.
-     */
-
-    void dispatchCompute(const glm::uvec3 &num_groups);
-    void dispatchCompute(GLuint num_groups_x, GLuint num_groups_y, GLuint num_groups_z);
-
-    /**
-     * Directly maps to glMemoryBarrier.
-     * See here https://www.opengl.org/sdk/docs/man/html/glMemoryBarrier.xhtml
-     *
-     * The 2 most common barries have their own function, see below.
-     */
-
-    static void memoryBarrier(MemoryBarrierMask barriers);
-
-    /**
-     * Calls memoryBarrier with GL_TEXTURE_FETCH_BARRIER_BIT.
-     *
-     * Texture fetches from shaders, including fetches from buffer object memory via buffer textures,
-     * after the barrier will reflect data written by shaders prior to the barrier.
-     */
-
-    static void memoryBarrierTextureFetch();
-
-    /**
-     * Calls memoryBarrier with GL_SHADER_IMAGE_ACCESS_BARRIER_BIT.
-     *
-     * Memory accesses using shader image load, store, and atomic built-in functions issued after the barrier
-     * will reflect data written by shaders prior to the barrier. Additionally, image stores and atomics issued
-     * after the barrier will not execute until all memory accesses (e.g., loads, stores, texture fetches, vertex fetches)
-     * initiated prior to the barrier complete.
-     */
-
-    static void memoryBarrierImageAccess();
-
-    // ===================================== uniforms =====================================
-
-    GLint getUniformLocation(const char* name);
-    void getUniformInfo(GLuint location);
-    virtual void checkUniforms(){}
+	Shader();
+	virtual ~Shader();
+	Shader(Shader const&) = delete;
+	Shader& operator=(Shader const&) = delete;
 
 
+	// ===================================== program stuff =====================================
 
-    // ===================================== uniform blocks =====================================
+	/**
+	* Creates the underlying gl-program, attaches all shader parts to it and links the shader.
+	* The ShaderParts have to be compiled without errors before this step.
+	* Returns the program id or 0 if the link fails.
+	*
+	* Calls checkUniforms after succesfully linking.
+	*/
+	GLuint createProgram();
 
-    GLuint getUniformBlockLocation(const char* name);
-    void setUniformBlockBinding(GLuint blockLocation, GLuint bindingPoint);
-    //size of the complete block in bytes
-    GLint getUniformBlockSize(GLuint blockLocation);
-    std::vector<GLint> getUniformBlockIndices(GLuint blockLocation);
-    std::vector<GLint> getUniformBlockSize(std::vector<GLint> indices);
-    std::vector<GLint> getUniformBlockType(std::vector<GLint> indices);
-    std::vector<GLint> getUniformBlockOffset(std::vector<GLint> indices);
+	/**
+	* Deletes the underlying gl-program. The shaderparts are not deleted, so it could be linked again.
+	*/
+	void destroyProgram();
+
+	/**
+	* Checks the programs info logs and returns true when no error has occured.
+	*/
+	bool printProgramLog();
+
+	/**
+	* Binds this shader to opengl. Maps to glUseProgram
+	*/
+	void bind();
+
+	/**
+	* Binds the default program (0) to opengl.
+	* Does nothing in testing and release mode.
+	*/
+	void unbind();
+
+	/**
+	* Returns true if this shader is the last shader that has called bind().
+	*/
+	bool isBound();
 
 
-    // ===================================== uniform uploads =====================================
+	// ===================================== Compute Shaders =====================================
+	// Note: Compute shaders require OpenGL 4.3+
 
-    void upload(int location, const mat4 &m);
-    void upload(int location, const vec4 &v);
-    void upload(int location, const vec3 &v);
-    void upload(int location, const vec2 &v);
-    void upload(int location, const int &v);
-    void upload(int location, const float &f);
-    //array uploads
-    void upload(int location, int count, mat4* m);
-    void upload(int location, int count, vec4* v);
-    void upload(int location, int count, vec3* v);
-    void upload(int location, int count, vec2* v);
+	/**
+	 * Returns the work group size specified in the shader.
+	 *
+	 * For example:
+	 * layout(local_size_x = 32, local_size_y = 32) in;
+	 *
+	 * returns glm::uvec3(32,32,1)
+	 */
 
-    //binds the texture to the given texture unit and sets the uniform.
-    void upload(int location, raw_Texture *texture, int textureUnit);
+	glm::uvec3 getComputeWorkGroupSize();
+
+	/**
+	 * Calculates the number of groups required for the given problem size.
+	 * The number of shader executions will then be greater or equal to the problem size.
+	 */
+
+	glm::uvec3 getNumGroupsCeil(const glm::uvec3 &problem_size);
+	glm::uvec3 getNumGroupsCeil(const glm::uvec3 &problem_size, const glm::uvec3 &work_group_size);
+
+	/**
+	 * Initates the compute operation.
+	 * The shader must be bound beforehand.
+	 */
+
+	void dispatchCompute(const glm::uvec3 &num_groups);
+	void dispatchCompute(GLuint num_groups_x, GLuint num_groups_y, GLuint num_groups_z);
+
+	/**
+	 * Directly maps to glMemoryBarrier.
+	 * See here https://www.opengl.org/sdk/docs/man/html/glMemoryBarrier.xhtml
+	 *
+	 * The 2 most common barries have their own function, see below.
+	 */
+
+	static void memoryBarrier(MemoryBarrierMask barriers);
+
+	/**
+	 * Calls memoryBarrier with GL_TEXTURE_FETCH_BARRIER_BIT.
+	 *
+	 * Texture fetches from shaders, including fetches from buffer object memory via buffer textures,
+	 * after the barrier will reflect data written by shaders prior to the barrier.
+	 */
+
+	static void memoryBarrierTextureFetch();
+
+	/**
+	 * Calls memoryBarrier with GL_SHADER_IMAGE_ACCESS_BARRIER_BIT.
+	 *
+	 * Memory accesses using shader image load, store, and atomic built-in functions issued after the barrier
+	 * will reflect data written by shaders prior to the barrier. Additionally, image stores and atomics issued
+	 * after the barrier will not execute until all memory accesses (e.g., loads, stores, texture fetches, vertex fetches)
+	 * initiated prior to the barrier complete.
+	 */
+
+	static void memoryBarrierImageAccess();
+
+	// ===================================== uniforms =====================================
+
+	GLint getUniformLocation(const char* name);
+	void getUniformInfo(GLuint location);
+	virtual void checkUniforms(){}
+
+
+
+	// ===================================== uniform blocks =====================================
+
+	GLuint getUniformBlockLocation(const char* name);
+	void setUniformBlockBinding(GLuint blockLocation, GLuint bindingPoint);
+	//size of the complete block in bytes
+	GLint getUniformBlockSize(GLuint blockLocation);
+	std::vector<GLint> getUniformBlockIndices(GLuint blockLocation);
+	std::vector<GLint> getUniformBlockSize(std::vector<GLint> indices);
+	std::vector<GLint> getUniformBlockType(std::vector<GLint> indices);
+	std::vector<GLint> getUniformBlockOffset(std::vector<GLint> indices);
+
+
+	// ===================================== uniform uploads =====================================
+
+	/**
+	* Uploading a uniform to video ram.
+	* Maps to glUniform**.
+	* The shader must be bound beforehand.
+	*/
+
+	//basic uploads
+	void upload(int location, const mat4 &m);
+	void upload(int location, const vec4 &v);
+	void upload(int location, const vec3 &v);
+	void upload(int location, const vec2 &v);
+	void upload(int location, const int &v);
+	void upload(int location, const float &f);
+	//array uploads
+	void upload(int location, int count, mat4* m);
+	void upload(int location, int count, vec4* v);
+	void upload(int location, int count, vec3* v);
+	void upload(int location, int count, vec2* v);
+
+	//binds the texture to the given texture unit and sets the uniform.
+	void upload(int location, raw_Texture *texture, int textureUnit);
 };
 
 
--- src/opengl/shader/shader.cpp
+++ src/opengl/shader/shader.cpp
@@ -3,267 +3,307 @@
 #include "saiga/util/error.h"
 #include <fstream>
 #include <algorithm>
+#include "saiga/util/assert.h"
+
+GLuint Shader::boundShader = 0;
 
 Shader::Shader(){
 }
 
 Shader::~Shader(){
-    destroyProgram();
+	destroyProgram();
 }
+
+// ===================================== program stuff =====================================
 
 
 GLuint Shader::createProgram(){
 
-    program = glCreateProgram();
+	program = glCreateProgram();
 
-    //attach all shaders
-    for(auto& sp : shaders){
-        glAttachShader(program,sp->id);
-    }
-    assert_no_glerror();
-    glLinkProgram(program);
-    printProgramLog();
+	//attach all shaders
+	for (auto& sp : shaders){
+		glAttachShader(program, sp->id);
+	}
+	assert_no_glerror();
+	glLinkProgram(program);
 
-    assert_no_glerror();
-    for(auto& sp : shaders){
-        sp->deleteGLShader();
-    }
+	if (!printProgramLog()){
+		return 0;
+	}
 
-    assert_no_glerror();
+	assert_no_glerror();
+	for (auto& sp : shaders){
+		sp->deleteGLShader();
+	}
 
-    checkUniforms();
+	assert_no_glerror();
 
-    assert_no_glerror();
-    return program;
+	checkUniforms();
+
+	assert_no_glerror();
+	return program;
 }
 
 void Shader::destroyProgram()
 {
-    if(program!=0){
-        glDeleteProgram(program);
-        program = 0;
-    }
+	if (program != 0){
+		glDeleteProgram(program);
+		program = 0;
+	}
+}
+
+bool Shader::printProgramLog(){
+	//Make sure name is shader
+	if (glIsProgram(program) == GL_TRUE)
+	{
+		//Program log length
+		int infoLogLength = 0;
+		int maxLength = infoLogLength;
+
+		//Get info std::string length
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
+
+		//Allocate std::string
+		char* infoLog = new char[maxLength];
+
+		//Get info log
+		glGetProgramInfoLog(program, maxLength, &infoLogLength, infoLog);
+		if (infoLogLength > 0)
+		{
+			//Print Log
+			std::cout << "program error:" << std::endl;
+			std::cout << infoLog << std::endl;
+		}
+
+		//Deallocate std::string
+		delete[] infoLog;
+		return infoLogLength == 0;
+	}
+	else
+	{
+		cout << "Name " << program << " is not a program" << endl;
+		return false;
+	}
 }
 
 void Shader::bind(){
-    if(program==0){
-        std::cerr<<"bind: no shader loaded!\n";
-        assert(0);
-        return;
-    }
-    glUseProgram(program);
+	assert(program);
+	//allow double bind
+	assert(boundShader == program || boundShader == 0);
+	boundShader = program;
+	glUseProgram(program);
 }
 
 void Shader::unbind(){
-    glUseProgram(0);
+	//allow double unbind
+	assert(boundShader == program || boundShader == 0);
+	boundShader = 0;
+#if defined(SAIGA_DEBUG)
+	glUseProgram(0);
+#endif
 }
 
-GLint Shader::getUniformLocation(const char* name){
-    GLint i = glGetUniformLocation(program,name);
-    return i;
+bool Shader::isBound(){
+	return boundShader == program;
 }
 
-void Shader::getUniformInfo(GLuint location){
-    const GLsizei bufSize = 128;
+// ===================================== Compute Shaders =====================================
 
-    GLsizei length;
-    GLint size;
-    GLenum type;
-    GLchar name[bufSize];
-
-    glGetActiveUniform(program,location,bufSize,&length,&size,&type,name);
-
-    cout<<"uniform info "<<location<<endl;
-    cout<<"name "<<name<<endl;
-    //    cout<<"length "<<length<<endl;
-    cout<<"size "<<size<<endl;
-    cout<<"type "<<type<<endl;
-}
-
-GLuint Shader::getUniformBlockLocation(const char *name)
-{
-    GLuint blockIndex = glGetUniformBlockIndex(program, name);
-
-    if(blockIndex==GL_INVALID_INDEX){
-        std::cerr<<"glGetUniformBlockIndex: uniform block invalid!"<<endl;
-    }
-    return blockIndex;
-}
-
-void Shader::setUniformBlockBinding(GLuint blockLocation, GLuint bindingPoint)
-{
-    glUniformBlockBinding(program, blockLocation, bindingPoint);
-}
-
-GLint Shader::getUniformBlockSize(GLuint blockLocation)
-{
-    GLint ret;
-    glGetActiveUniformBlockiv(program,blockLocation,GL_UNIFORM_BLOCK_DATA_SIZE,&ret);
-    return ret;
-}
-
-std::vector<GLint> Shader::getUniformBlockIndices(GLuint blockLocation)
-{
-    GLint ret;
-    glGetActiveUniformBlockiv(program,blockLocation,GL_UNIFORM_BLOCK_ACTIVE_UNIFORMS,&ret);
-
-    std::vector<GLint> indices(ret);
-    glGetActiveUniformBlockiv(program,blockLocation,GL_UNIFORM_BLOCK_ACTIVE_UNIFORM_INDICES,&indices[0]);
-
-    return indices;
-}
-
-std::vector<GLint> Shader::getUniformBlockSize(std::vector<GLint> indices)
-{
-    std::vector<GLint> ret(indices.size());
-    glGetActiveUniformsiv(program,indices.size(),(GLuint*)indices.data(),GL_UNIFORM_SIZE,ret.data());
-    return ret;
-}
-
-std::vector<GLint> Shader::getUniformBlockType(std::vector<GLint> indices)
-{
-    std::vector<GLint> ret(indices.size());
-    glGetActiveUniformsiv(program,indices.size(),(GLuint*)indices.data(),GL_UNIFORM_TYPE,ret.data());
-    return ret;
-}
-
-std::vector<GLint> Shader::getUniformBlockOffset(std::vector<GLint> indices)
-{
-    std::vector<GLint> ret(indices.size());
-    glGetActiveUniformsiv(program,indices.size(),(GLuint*)indices.data(),GL_UNIFORM_OFFSET,ret.data());
-    return ret;
-}
-
-void Shader::printProgramLog( ){
-    //Make sure name is shader
-    if( glIsProgram( program ) == GL_TRUE )
-    {
-        //Program log length
-        int infoLogLength = 0;
-        int maxLength = infoLogLength;
-
-        //Get info std::string length
-        glGetProgramiv( program, GL_INFO_LOG_LENGTH, &maxLength );
-
-        //Allocate std::string
-        char* infoLog = new char[ maxLength ];
-
-        //Get info log
-        glGetProgramInfoLog( program, maxLength, &infoLogLength, infoLog );
-        if( infoLogLength > 0 )
-        {
-            //Print Log
-            std::cout<<"program error:"<<std::endl;
-            std::cout<<  infoLog << std::endl;
-        }
-
-        //Deallocate std::string
-        delete[] infoLog;
-    }
-    else
-    {
-        cout<< "Name "<<program<<" is not a program"<<endl;
-    }
-}
 
 glm::uvec3 Shader::getComputeWorkGroupSize()
 {
-    GLint work_size[3];
-    glGetProgramiv(program, GL_COMPUTE_WORK_GROUP_SIZE, work_size);
-    return glm::uvec3(work_size[0],work_size[1],work_size[2]);
+	GLint work_size[3];
+	glGetProgramiv(program, GL_COMPUTE_WORK_GROUP_SIZE, work_size);
+	return glm::uvec3(work_size[0], work_size[1], work_size[2]);
 }
 
 glm::uvec3 Shader::getNumGroupsCeil(const glm::uvec3 &problem_size)
 {
-    return getNumGroupsCeil(problem_size,getComputeWorkGroupSize());
+	return getNumGroupsCeil(problem_size, getComputeWorkGroupSize());
 }
 
 glm::uvec3 Shader::getNumGroupsCeil(const glm::uvec3 &problem_size, const glm::uvec3 &work_group_size)
 {
-//    glm::uvec3 ret = problem_size/work_group_size;
-//    glm::uvec3 rest = problem_size%work_group_size;
+	//    glm::uvec3 ret = problem_size/work_group_size;
+	//    glm::uvec3 rest = problem_size%work_group_size;
 
-//    ret.x += rest.x ? 1 : 0;
-//    ret.y += rest.y ? 1 : 0;
-//    ret.z += rest.z ? 1 : 0;
+	//    ret.x += rest.x ? 1 : 0;
+	//    ret.y += rest.y ? 1 : 0;
+	//    ret.z += rest.z ? 1 : 0;
 
-//    return ret;
+	//    return ret;
 
-    return (problem_size+work_group_size-glm::uvec3(1))/(work_group_size);
+	return (problem_size + work_group_size - glm::uvec3(1)) / (work_group_size);
 }
 
 void Shader::dispatchCompute(const glm::uvec3 &num_groups)
 {
-    dispatchCompute(num_groups.x,num_groups.y,num_groups.z);
+	assert(isBound());
+	dispatchCompute(num_groups.x, num_groups.y, num_groups.z);
 }
 
 void Shader::dispatchCompute(GLuint num_groups_x, GLuint num_groups_y, GLuint num_groups_z)
 {
-    glDispatchCompute(num_groups_x, num_groups_y, num_groups_z);
+	assert(isBound());
+	glDispatchCompute(num_groups_x, num_groups_y, num_groups_z);
 }
+
+
 
 void Shader::memoryBarrier(MemoryBarrierMask barriers)
 {
-    glMemoryBarrier(barriers);
+	glMemoryBarrier(barriers);
 }
 
 void Shader::memoryBarrierTextureFetch()
 {
-    memoryBarrier(GL_TEXTURE_FETCH_BARRIER_BIT);
+	memoryBarrier(GL_TEXTURE_FETCH_BARRIER_BIT);
 }
 
 void Shader::memoryBarrierImageAccess()
 {
-    memoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+	memoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
 }
 
+// ===================================== uniforms =====================================
+
+
+GLint Shader::getUniformLocation(const char* name){
+	GLint i = glGetUniformLocation(program, name);
+	return i;
+}
+
+void Shader::getUniformInfo(GLuint location){
+	const GLsizei bufSize = 128;
+
+	GLsizei length;
+	GLint size;
+	GLenum type;
+	GLchar name[bufSize];
+
+	glGetActiveUniform(program, location, bufSize, &length, &size, &type, name);
+
+	cout << "uniform info " << location << endl;
+	cout << "name " << name << endl;
+	//    cout<<"length "<<length<<endl;
+	cout << "size " << size << endl;
+	cout << "type " << type << endl;
+}
+
+// ===================================== uniform blocks =====================================
+
+GLuint Shader::getUniformBlockLocation(const char *name)
+{
+	GLuint blockIndex = glGetUniformBlockIndex(program, name);
+
+	if (blockIndex == GL_INVALID_INDEX){
+		std::cerr << "glGetUniformBlockIndex: uniform block invalid!" << endl;
+	}
+	return blockIndex;
+}
+
+void Shader::setUniformBlockBinding(GLuint blockLocation, GLuint bindingPoint)
+{
+	glUniformBlockBinding(program, blockLocation, bindingPoint);
+}
+
+GLint Shader::getUniformBlockSize(GLuint blockLocation)
+{
+	GLint ret;
+	glGetActiveUniformBlockiv(program, blockLocation, GL_UNIFORM_BLOCK_DATA_SIZE, &ret);
+	return ret;
+}
+
+std::vector<GLint> Shader::getUniformBlockIndices(GLuint blockLocation)
+{
+	GLint ret;
+	glGetActiveUniformBlockiv(program, blockLocation, GL_UNIFORM_BLOCK_ACTIVE_UNIFORMS, &ret);
+
+	std::vector<GLint> indices(ret);
+	glGetActiveUniformBlockiv(program, blockLocation, GL_UNIFORM_BLOCK_ACTIVE_UNIFORM_INDICES, &indices[0]);
+
+	return indices;
+}
+
+std::vector<GLint> Shader::getUniformBlockSize(std::vector<GLint> indices)
+{
+	std::vector<GLint> ret(indices.size());
+	glGetActiveUniformsiv(program, indices.size(), (GLuint*)indices.data(), GL_UNIFORM_SIZE, ret.data());
+	return ret;
+}
+
+std::vector<GLint> Shader::getUniformBlockType(std::vector<GLint> indices)
+{
+	std::vector<GLint> ret(indices.size());
+	glGetActiveUniformsiv(program, indices.size(), (GLuint*)indices.data(), GL_UNIFORM_TYPE, ret.data());
+	return ret;
+}
+
+std::vector<GLint> Shader::getUniformBlockOffset(std::vector<GLint> indices)
+{
+	std::vector<GLint> ret(indices.size());
+	glGetActiveUniformsiv(program, indices.size(), (GLuint*)indices.data(), GL_UNIFORM_OFFSET, ret.data());
+	return ret;
+}
+
+// ===================================== uniform uploads =====================================
 
 
 void Shader::upload(int location, const mat4 &m){
-    glUniformMatrix4fv(location,1,GL_FALSE, (GLfloat*)&m[0]);
+	assert(isBound());
+	glUniformMatrix4fv(location, 1, GL_FALSE, (GLfloat*)&m[0]);
 }
 
 void Shader::upload(int location, const vec4 &v){
-    glUniform4fv(location,1,(GLfloat*)&v[0]);
+	assert(isBound());
+	glUniform4fv(location, 1, (GLfloat*)&v[0]);
 }
 
 void Shader::upload(int location, const vec3 &v){
-    glUniform3fv(location,1,(GLfloat*)&v[0]);
+	assert(isBound());
+	glUniform3fv(location, 1, (GLfloat*)&v[0]);
 }
 
 void Shader::upload(int location, const vec2 &v){
-    glUniform2fv(location,1,(GLfloat*)&v[0]);
+	assert(isBound());
+	glUniform2fv(location, 1, (GLfloat*)&v[0]);
 }
 
 void Shader::upload(int location, const int &i){
-    glUniform1i(location,(GLint)i);
+	assert(isBound());
+	glUniform1i(location, (GLint)i);
 }
 
 void Shader::upload(int location, const float &f){
-    glUniform1f(location,(GLfloat)f);
+	assert(isBound());
+	glUniform1f(location, (GLfloat)f);
 }
 
 
 void Shader::upload(int location, int count, mat4* m){
-    glUniformMatrix4fv(location,count,GL_FALSE,(GLfloat*)m);
+	assert(isBound());
+	glUniformMatrix4fv(location, count, GL_FALSE, (GLfloat*)m);
 }
 
 void Shader::upload(int location, int count, vec4* v){
-    glUniform4fv(location,count,(GLfloat*)v);
+	assert(isBound());
+	glUniform4fv(location, count, (GLfloat*)v);
 }
 
 void Shader::upload(int location, int count, vec3* v){
-    glUniform3fv(location,count,(GLfloat*)v);
+	assert(isBound());
+	glUniform3fv(location, count, (GLfloat*)v);
 }
 
 void Shader::upload(int location, int count, vec2* v){
-    glUniform2fv(location,count,(GLfloat*)v);
+	assert(isBound());
+	glUniform2fv(location, count, (GLfloat*)v);
 }
 
 void Shader::upload(int location, raw_Texture *texture, int textureUnit)
 {
-    texture->bind(textureUnit);
-    Shader::upload(location,textureUnit);
+	assert(isBound());
+	texture->bind(textureUnit);
+	Shader::upload(location, textureUnit);
 }
-
-
