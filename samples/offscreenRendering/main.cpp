bcc9f7e28b6238dad82f0813284049e3a78b2914
Darius Rueckert <darius.rueckert@fau.de>
2017-06-14 19:15:47 +0200

    offscreen sample

---
+++ samples/offscreenRendering/main.cpp
@@ -0,0 +1,42 @@
+#include "saiga/framework.h"
+#include "saiga/egl/offscreen_window.h"
+#include "saiga/rendering/deferred_renderer.h"
+#include "saiga/util/crash.h"
+
+#include "simpleWindow.h"
+
+int main( int argc, char* args[] )
+{
+    //Add a signal handler for SIGSEGV and print the stack trace when a SIGSEGV is caught
+    catchSegFaults();
+
+    //Specify some window parameters and create a SDL window
+    WindowParameters windowParameters;
+    windowParameters.name = "Simple SDL Window";
+    windowParameters.vsync = false;
+    windowParameters.resizeAble = true;
+    windowParameters.mode = WindowParameters::Mode::windowed;
+    windowParameters.width = 1280;
+    windowParameters.height = 720;
+    //make one screenshot per second and save it in the debug directory
+    windowParameters.debugScreenshotPath = "debug/";
+    windowParameters.debugScreenshotTime = 1.0f;
+
+    auto window = new OffscreenWindow(windowParameters);
+
+    //Create the deferred rendering engine
+    RenderingParameters rp; //Use default rendering settings
+    window->init(rp);
+
+    //Everything from saiga is now setup, so we can start our own program now
+    SimpleWindow* simpleWindow = new SimpleWindow(window);
+
+    int updatesPerSecond = 60;
+    int framesPerSecond = 0; //no limit
+    window->startMainLoop(updatesPerSecond,framesPerSecond);
+
+    delete simpleWindow;
+    delete window;
+
+    return 0;
+}
---
+++ samples/offscreenRendering/simpleWindow.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "saiga/rendering/renderer.h"
+#include "saiga/world/proceduralSkybox.h"
+
+#include "saiga/assets/all.h"
+#include "saiga/assets/objAssetLoader.h"
+
+#include "saiga/sdl/sdl_eventhandler.h"
+#include "saiga/sdl/sdl_camera.h"
+#include "saiga/sdl/sdl_window.h"
+
+#include "saiga/rendering/lighting/directional_light.h"
+
+class SimpleWindow : public Program
+{
+public:
+    SDLCamera<PerspectiveCamera> camera;
+
+    SimpleAssetObject cube1, cube2;
+    SimpleAssetObject groundPlane;
+    SimpleAssetObject sphere;
+
+    ProceduralSkybox skybox;
+
+    std::shared_ptr<DirectionalLight> sun;
+
+    SimpleWindow(OpenGLWindow* window);
+    ~SimpleWindow();
+
+    void update(float dt) override;
+    void interpolate(float dt, float interpolation) override;
+    void render(Camera *cam) override;
+    void renderDepth(Camera *cam) override;
+    void renderOverlay(Camera *cam) override;
+    void renderFinal(Camera *cam) override;
+
+};
+
+
