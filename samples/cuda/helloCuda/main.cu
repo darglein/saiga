7dca36567dd7e464e285523f5c0db6d2788190be
Darius Rueckert <darius.rueckert@fau.de>
2018-08-24 17:26:38 +0200

    cuda samples

---
+++ samples/cuda/helloCuda/main.cu
@@ -0,0 +1,22 @@
+﻿/**
+ * Copyright (c) 2017 Darius Rückert
+ * Licensed under the MIT License.
+ * See LICENSE file for more information.
+ */
+
+#include <iostream>
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+__global__ void helloCudaKernel()
+{
+    printf("Hello from CUDA!\n");
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello CUDA?" << std::endl;
+    helloCudaKernel<<<1,1>>>();
+    cudaDeviceSynchronize();
+    return 0;
+}
