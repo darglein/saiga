6eeae3d06668b3ce576837bfb6356826033f648f
Darius Rueckert <darius.rueckert@fau.de>
2018-09-10 12:29:42 +0200

    two more cuda samples

---
+++ samples/cuda/eventSync/main.cu
@@ -0,0 +1,104 @@
+﻿/**
+ * Copyright (c) 2017 Darius Rückert
+ * Licensed under the MIT License.
+ * See LICENSE file for more information.
+ */
+
+#include <iostream>
+#include <vector>
+#include "saiga/util/glm.h"
+#include <thrust/device_vector.h>
+#include "saiga/cuda/cudaHelper.h"
+#include "saiga/cuda/device_helper.h"
+#include "saiga/cuda/pinned_vector.h"
+#include "saiga/cuda/stream.h"
+#include "saiga/cuda/event.h"
+
+using Saiga::ArrayView;
+using Saiga::CUDA::ThreadInfo;
+
+
+
+template<int K> __global__ static
+void A(ArrayView<int> data)
+{
+    ThreadInfo<> ti;
+    if(ti.thread_id >= data.size()) return;
+    for(int i = 0; i < K; ++i) atomicAdd(&data[ti.thread_id],1);
+}
+
+template<int K> __global__ static
+void B(ArrayView<int> data)
+{
+    ThreadInfo<> ti;
+    if(ti.thread_id >= data.size()) return;
+    for(int i = 0; i < K; ++i) atomicAdd(&data[ti.thread_id],1);
+}
+
+template<int K> __global__ static
+void C(ArrayView<int> data)
+{
+    ThreadInfo<> ti;
+    if(ti.thread_id >= data.size()) return;
+    for(int i = 0; i < K; ++i) atomicAdd(&data[ti.thread_id],1);
+}
+
+template<int K> __global__ static
+void D(ArrayView<int> data)
+{
+    ThreadInfo<> ti;
+    if(ti.thread_id >= data.size()) return;
+    for(int i = 0; i < K; ++i) atomicAdd(&data[ti.thread_id],1);
+}
+
+static void uploadProcessDownloadAsync(int N)
+{
+
+    thrust::device_vector<int> d_data(N);
+    thrust::device_vector<int> d_data2(N);
+    thrust::device_vector<int> d_data3(N);
+
+    size_t size = N * sizeof(int);
+
+
+    // Compute launch arguments
+    const unsigned int BLOCK_SIZE = 128;
+    const unsigned int BLOCKS = Saiga::CUDA::getBlockCount(N,BLOCK_SIZE);
+
+    // Create a separate stream for each slice for maximum parallelism
+    Saiga::CUDA::CudaStream stream1, stream2;
+
+    {
+
+        CUDA_SYNC_CHECK_ERROR();
+
+        A<1024 * 2><<<BLOCKS,BLOCK_SIZE,0,stream1>>>(d_data);
+        B<1024><<<BLOCKS,BLOCK_SIZE,0,stream1>>>(d_data2);
+
+        C<1024><<<BLOCKS,BLOCK_SIZE,0,stream2>>>(d_data3);
+        D<1024><<<BLOCKS,BLOCK_SIZE,0,stream2>>>(d_data);
+
+        CUDA_SYNC_CHECK_ERROR();
+
+        Saiga::CUDA::CudaEvent event;
+
+        A<1024 * 2><<<BLOCKS,BLOCK_SIZE,0,stream1>>>(d_data);
+        event.record(stream1);
+        B<1024><<<BLOCKS,BLOCK_SIZE,0,stream1>>>(d_data2);
+
+        C<1024><<<BLOCKS,BLOCK_SIZE,0,stream2>>>(d_data3);
+        stream2.waitForEvent(event);
+        D<1024><<<BLOCKS,BLOCK_SIZE,0,stream2>>>(d_data);
+
+
+        CUDA_SYNC_CHECK_ERROR();
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    uploadProcessDownloadAsync(1024 * 8);
+
+    cout << "Done." << endl;
+}
+
---
+++ samples/cuda/intraBlock/main.cu
@@ -0,0 +1,120 @@
+﻿/**
+ * Copyright (c) 2017 Darius Rückert
+ * Licensed under the MIT License.
+ * See LICENSE file for more information.
+ */
+
+#include <iostream>
+#include <vector>
+#include "saiga/util/glm.h"
+#include <thrust/device_vector.h>
+#include "saiga/cuda/cudaHelper.h"
+#include "saiga/cuda/device_helper.h"
+#include "saiga/cuda/pinned_vector.h"
+#include "saiga/cuda/stream.h"
+#include "saiga/cuda/event.h"
+#include "saiga/cuda/memory.h"
+
+using Saiga::ArrayView;
+using Saiga::CUDA::ThreadInfo;
+
+
+__device__ inline
+void computeBeforeSync(){  }
+
+__device__ inline
+void computeAfterSync(int previousBlockValue){  }
+
+
+template<unsigned int BLOCK_SIZE>
+__global__ static
+void ibs(ArrayView<int> data, ArrayView<int> blockTmp, ArrayView<unsigned int> atomics)
+{
+    ThreadInfo<BLOCK_SIZE> ti;
+    if(ti.thread_id >= data.size()) return;
+
+    __shared__ unsigned int blockId;
+    __shared__ int previousBlockValue;
+
+    if(ti.local_thread_id == 0)
+    {
+        blockId = atomicInc(&atomics[0],0xFFFFFFFF);
+    }
+
+    __syncthreads();
+
+
+    computeBeforeSync();
+
+    if(ti.local_thread_id == 0)
+    {
+        // wait until previous block is finished
+
+        // does not work
+//        while(atomics[1] != realBlockId)
+//        while(Saiga::CUDA::loadNoL1Cache(&atomics[1]) != realBlockId)
+        while(atomicAdd(&atomics[1],0) != blockId)
+        {
+
+        }
+        previousBlockValue = blockId == 0 ? 0 : blockTmp[blockId-1];
+    }
+
+
+
+    __syncthreads();
+
+
+    // do something
+    computeAfterSync(previousBlockValue);
+
+    int computedBlockValue = previousBlockValue + blockId;
+
+    __syncthreads();
+
+    if(ti.local_thread_id == 0)
+    {
+        blockTmp[blockId] = computedBlockValue;
+        // Increment wait counter so the next block can ru
+//        atomicInc(&atomics[1],0xFFFFFFFF);
+        atomics[1] = blockId + 1;
+    }
+}
+
+static void intraBlockTest(int N)
+{
+
+
+
+    // Compute launch arguments
+    const unsigned int BLOCK_SIZE = 128;
+    const unsigned int BLOCKS = Saiga::CUDA::getBlockCount(N,BLOCK_SIZE);
+
+
+
+    thrust::device_vector<int> d_data(N);
+    thrust::device_vector<int> d_blockTmp(BLOCKS);
+    thrust::device_vector<unsigned int> atomics(2,0);
+
+
+    ibs<BLOCK_SIZE><<<BLOCKS,BLOCK_SIZE>>>(d_data,d_blockTmp,atomics);
+
+
+    CUDA_SYNC_CHECK_ERROR();
+
+    thrust::host_vector<int> data = d_blockTmp;
+
+    for(auto i : data)
+        cout << i << endl;
+
+
+
+}
+
+int main(int argc, char *argv[])
+{
+    intraBlockTest(1024 * 2);
+
+    cout << "Done." << endl;
+}
+
