37066896e0c21bdf17b58cd48afe91a366f6ccd2
Darius Rueckert <darius.rueckert@fau.de>
2018-09-20 16:02:15 +0200

    missing file

---
+++ src/saiga/image/floatTexels.h
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2017 Darius Rückert 
+ * Licensed under the MIT License.
+ * See LICENSE file for more information.
+ */
+
+#pragma once
+
+#include "saiga/config.h"
+#include "saiga/util/glm.h"
+#include "saiga/image/imageFormat.h"
+
+namespace Saiga {
+
+
+template<typename T>
+struct SAIGA_GLOBAL MatchingFloatType{ using FloatType = T; };
+
+template<> struct MatchingFloatType<ucvec3>{using FloatType = vec3; };
+template<> struct MatchingFloatType<ucvec4>{using FloatType = vec4; };
+
+
+
+template<typename T, typename ST = float>
+struct SAIGA_GLOBAL NormalizeScale{ const static ST scale = ST(1); };
+
+template<typename ST> struct NormalizeScale<char,ST>{ constexpr static ST scale = ST(255); };
+template<typename ST> struct NormalizeScale<unsigned char,ST>{ constexpr static ST scale = ST(255); };
+
+
+
+
+template<typename T, bool normalize = false>
+struct SAIGA_GLOBAL TexelFloatConverter
+{
+    using ITT = ImageTypeTemplate<T>;
+    using TexelType = T;
+    using NS = NormalizeScale<typename ITT::ChannelType>;
+    using FloatType = typename MatchingFloatType<T>::FloatType;
+
+
+    FloatType toFloat(TexelType t)
+    {
+        return normalize ? FloatType(t) / NS::scale : FloatType(t);
+    }
+
+    TexelType fromFloat(FloatType f)
+    {
+//        return TexelType(f);
+        return normalize ? TexelType(f * NS::scale) : TexelType(f);
+    }
+
+};
+
+
+
+
+}
