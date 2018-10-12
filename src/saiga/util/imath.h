d6d1d952d50c2144bd5ebf607cb7e6972ad6935a
Peter Eichinger <peter.eichinger@gmail.com>
2018-10-12 13:36:42 +0200

    Make iAlignDown and Up into templated functions and clean up code

--- src/saiga/util/imath.h
+++ src/saiga/util/imath.h
@@ -7,6 +7,7 @@
 #pragma once
 
 #include "saiga/config.h"
+#include <type_traits>
 
 namespace Saiga {
 
@@ -17,29 +18,58 @@ int iDivUp(int a, int b) { return (a + b - 1) / b; }
 HD SAIGA_CONSTEXPR inline
 int iDivDown(int a, int b) { return a / b; }
 
-//finds the smallest number that is bigger or equal than a and divisible by b
-HD SAIGA_CONSTEXPR inline
-int iAlignUp(int a, int b) { return (a % b != 0) ?  (a - a % b + b) : a; }
 
-//finds the largest number that is smaller or equal than a and divisible by b
+/**
+ * Align the value \p a to the next multiple of \p b
+ * @tparam T Type of the value to align
+ * @tparam U Type of the value to align to
+ * @param a Value to align
+ * @param b Value of alignment
+ * @return The smallest multiple of \p b that is not less than \p a
+ */
+template<typename T, typename U>
 HD SAIGA_CONSTEXPR inline
-int iAlignDown(int a, int b) {return a - a % b; }
+T iAlignUp(T a, U b) {
+    static_assert(std::is_integral<T>::value &&
+                  std::is_integral<U>::value,
+                  "only applicable to integral types");
+    return (a % b != 0) ? (a - a % b + b) : a;
+}
+
+/**
+ * finds the largest number that is smaller or equal than a and divisible by b
+ * @tparam T Type of the value to align
+ * @tparam U Type of the value to align to
+ * @param a Value to align
+ * @param b Value of alignment
+ * @return The largest multiple of \p b that is not greater than \p a
+ */
+//
+template<typename T, typename U>
+HD SAIGA_CONSTEXPR inline
+T iAlignDown(T a, U b) {
+    static_assert(std::is_integral<T>::value &&
+                  std::is_integral<U>::value,
+                  "only applicable to integral types");
+    return a - a % b;
+}
+
 
 HD inline
-int iFloor(float value){
-    int i = (int)value;
+int iFloor(float value) {
+    int i = (int) value;
     return i - (i > value);
 }
 
 HD inline
-int iCeil(float value){
-    int i = (int)value;
+int iCeil(float value) {
+    int i = (int) value;
     return i + (i < value);
 }
 
 HD inline
-int iRound(float value){
-    return (int)(value + (value >= 0 ? 0.5f : -0.5f));
+int iRound(float value) {
+    return (int) (value + (value >= 0 ? 0.5f : -0.5f));
 }
 
 
