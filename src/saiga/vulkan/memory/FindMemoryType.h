d61e98254aea754bc6a5c3fc7dede5eb742430b1
Peter Eichinger <peter.eichinger@gmail.com>
2018-10-25 16:41:54 +0200

    Add allocator implementation for vulkan

---
+++ src/saiga/vulkan/memory/FindMemoryType.h
@@ -0,0 +1,19 @@
+//
+// Created by Peter Eichinger on 25.10.18.
+//
+
+#pragma once
+
+#include <vulkan/vulkan.hpp>
+
+uint32_t findMemoryType(vk::PhysicalDevice _pDev, uint32_t typeFilter, const vk::MemoryPropertyFlags &properties) {
+    vk::PhysicalDeviceMemoryProperties memProperties = _pDev.getMemoryProperties();
+
+    for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+        if (typeFilter & (1 << i) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+            return i;
+        }
+    }
+
+    throw std::runtime_error("failed to find suitable memory type!");
+}
