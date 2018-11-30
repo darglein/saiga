5ce503d43f228017efc0366108b78492aa035710
Peter Eichinger <peter.eichinger@gmail.com>
2018-11-30 13:52:47 +0100

    Add missing implementation file VulkanMemory

---
+++ src/saiga/vulkan/memory/VulkanMemory.cpp
@@ -0,0 +1,78 @@
+//
+// Created by Peter Eichinger on 2018-11-30.
+//
+
+#include "VulkanMemory.h"
+
+void VulkanMemory::init(vk::PhysicalDevice _pDevice, vk::Device _device) {
+    m_pDevice = _pDevice;
+    m_device  = _device;
+    strategy  = FirstFitStrategy();
+    chunkAllocator.init(_pDevice, _device);
+
+    auto vertIndexType = BufferType{vk::BufferUsageFlagBits::eVertexBuffer | vk::BufferUsageFlagBits::eIndexBuffer |
+                                    vk::BufferUsageFlagBits::eTransferDst,
+                                    vk::MemoryPropertyFlagBits::eDeviceLocal};
+    bufferAllocators.emplace(
+            vertIndexType, BufferChunkAllocator(m_device, &chunkAllocator, vertIndexType.memoryFlags,
+                                                vertIndexType.usageFlags, strategy, fallback_buffer_chunk_size, false));
+}
+
+VulkanMemory::BufferIter
+VulkanMemory::createNewBufferAllocator(VulkanMemory::BufferMap &map, const VulkanMemory::BufferDefaultMap &defaultSizes,
+                                       const VulkanMemory::BufferType &type) {
+    auto found = find_default_size<BufferDefaultMap, BufferType>(default_buffer_chunk_sizes, type);
+
+    bool mapped =(type.memoryFlags & vk::MemoryPropertyFlagBits::eHostVisible) == vk::MemoryPropertyFlagBits::eHostVisible;
+    auto emplaced = map.emplace(type, BufferChunkAllocator(m_device, &chunkAllocator, type.memoryFlags,
+                                                           type.usageFlags, strategy, found->second, mapped));
+    SAIGA_ASSERT(emplaced.second, "Allocator was already present.");
+    return emplaced.first;
+}
+
+VulkanMemory::ImageIter
+VulkanMemory::createNewImageAllocator(VulkanMemory::ImageMap &map, const VulkanMemory::ImageDefaultMap &defaultSizes,
+                                      const VulkanMemory::ImageType &type) {
+    auto found = find_default_size<ImageDefaultMap, ImageType>(default_image_chunk_sizes, type);
+    bool mapped =(type.memoryFlags & vk::MemoryPropertyFlagBits::eHostVisible) == vk::MemoryPropertyFlagBits::eHostVisible;
+
+    auto emplaced = map.emplace(
+            type, ImageChunkAllocator(m_device, &chunkAllocator, type.memoryFlags, strategy, found->second, mapped));
+    SAIGA_ASSERT(emplaced.second, "Allocator was already present.");
+    return emplaced.first;
+}
+
+BaseMemoryAllocator &VulkanMemory::getAllocator(const vk::BufferUsageFlags &usage, const vk::MemoryPropertyFlags &flags) {
+    auto foundAllocator = findAllocator<BufferMap, vk::BufferUsageFlags>(bufferAllocators, {usage, flags});
+    if (foundAllocator == bufferAllocators.end())
+    {
+        foundAllocator = createNewBufferAllocator(bufferAllocators, default_buffer_chunk_sizes, {usage, flags});
+    }
+    return foundAllocator->second;
+}
+
+BaseMemoryAllocator &
+VulkanMemory::getImageAllocator(const vk::MemoryPropertyFlags &flags, const vk::ImageUsageFlags &usage) {
+    auto foundAllocator = findAllocator<ImageMap, vk::ImageUsageFlags>(imageAllocators, {usage, flags});
+
+    if (foundAllocator == imageAllocators.end())
+    {
+        foundAllocator = createNewImageAllocator(imageAllocators, default_image_chunk_sizes, {usage, flags});
+    }
+
+    return foundAllocator->second;
+}
+
+void VulkanMemory::destroy() {
+    chunkAllocator.destroy();
+
+    for (auto& allocator : bufferAllocators)
+    {
+        allocator.second.destroy();
+    }
+
+    for (auto& allocator : imageAllocators)
+    {
+        allocator.second.destroy();
+    }
+}
