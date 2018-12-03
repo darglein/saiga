8a174bf3efd72f5510ccadde4a4549992108cdb6
Peter Eichinger <peter.eichinger@gmail.com>
2018-12-03 16:35:43 +0100

    Cleanup queue

--- src/saiga/vulkan/Queue.cpp
+++ src/saiga/vulkan/Queue.cpp
@@ -7,17 +7,18 @@
 #include "Queue.h"
 #include "saiga/util/easylogging++.h"
 
-namespace Saiga {
-namespace Vulkan {
-
+namespace Saiga
+{
+namespace Vulkan
+{
 void Queue::create(vk::Device _device, uint32_t _queueFamilyIndex, uint32_t _queueIndex)
 {
-    device = _device;
+    device           = _device;
     queueFamilyIndex = _queueFamilyIndex;
-    queueIndex = _queueIndex;
-    queue = _device.getQueue(_queueFamilyIndex,_queueIndex);
+    queueIndex       = _queueIndex;
+    queue            = _device.getQueue(_queueFamilyIndex, _queueIndex);
     SAIGA_ASSERT(queue);
-    commandPool.create(device,_queueFamilyIndex);
+    commandPool.create(device, _queueFamilyIndex);
 }
 
 void Queue::waitIdle()
@@ -29,15 +30,16 @@ void Queue::destroy()
 {
     waitIdle();
 
-    LOG(INFO) << "Destroying queue with " << commandPools.size() << " command pools" ;
-    for(auto& pool : commandPools) {
-        LOG(INFO) << "Destroying command pool: "<<static_cast<vk::CommandPool>(pool);
+    LOG(INFO) << "Destroying queue with " << commandPools.size() << " command pools";
+    for (auto& pool : commandPools)
+    {
+        LOG(INFO) << "Destroying command pool: " << static_cast<vk::CommandPool>(pool);
         pool.destroy();
     }
 
     commandPool.destroy();
 
-//    device.destroy(*this);
+    //    device.destroy(*this);
 }
 
 void Queue::submitAndWait(vk::CommandBuffer cmd)
@@ -48,43 +50,46 @@ void Queue::submitAndWait(vk::CommandBuffer cmd)
     }
     vk::SubmitInfo submitInfo;
     submitInfo.commandBufferCount = 1;
-    submitInfo.pCommandBuffers = &cmd;
-    auto fence = device.createFence({});
+    submitInfo.pCommandBuffers    = &cmd;
+    vk::FenceCreateInfo fci{};
     submitMutex.lock();
-    queue.submit(submitInfo,fence);
+    auto fence = device.createFence(fci);
+    queue.submit(submitInfo, fence);
     submitMutex.unlock();
     device.waitForFences(fence, VK_TRUE, std::numeric_limits<uint64_t>::max());
     device.destroyFence(fence);
 }
 
 
-vk::Fence Queue::submit(vk::CommandBuffer cmd) {
+vk::Fence Queue::submit(vk::CommandBuffer cmd)
+{
     SAIGA_ASSERT(cmd, "invalid command buffer provided");
 
     vk::SubmitInfo submitInfo;
     submitInfo.commandBufferCount = 1;
-    submitInfo.pCommandBuffers = &cmd;
-    auto fence = device.createFence({});
+    submitInfo.pCommandBuffers    = &cmd;
+    auto fence                    = device.createFence({});
     submitMutex.lock();
-    queue.submit(submitInfo,fence);
+    queue.submit(submitInfo, fence);
     submitMutex.unlock();
     return fence;
 }
 
-CommandPool Queue::createCommandPool() {
+CommandPool Queue::createCommandPool()
+{
     commandPoolCreationMutex.lock();
 
     CommandPool newCommandPool;
-    newCommandPool.create(device,queueFamilyIndex);
+    newCommandPool.create(device, queueFamilyIndex);
     LOG(INFO) << "Creating command pool: " << static_cast<vk::CommandPool>(newCommandPool);
-//    LOG(INFO) << commandPools.size();
+    //    LOG(INFO) << commandPools.size();
     commandPools.push_back(newCommandPool);
-//    LOG(INFO) << commandPools.size();
+    //    LOG(INFO) << commandPools.size();
     commandPoolCreationMutex.unlock();
     return newCommandPool;
 }
 
 
 
-}
-}
+}  // namespace Vulkan
+}  // namespace Saiga
