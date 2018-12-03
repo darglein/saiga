0dde9ba5b65082f3a10a9978df26097b910eaaa5
Peter Eichinger <peter.eichinger@gmail.com>
2018-12-03 22:17:48 +0100

    Cleanup

--- src/saiga/vulkan/memory/FitStrategy.cpp
+++ src/saiga/vulkan/memory/FitStrategy.cpp
@@ -2,33 +2,36 @@
 // Created by Peter Eichinger on 30.10.18.
 //
 
-#include <utility>
 #include "FitStrategy.h"
 #include <tuple>
-namespace Saiga{
-namespace Vulkan{
-namespace Memory{
+#include <utility>
+namespace Saiga
+{
+namespace Vulkan
+{
+namespace Memory
+{
+std::pair<ChunkIterator, LocationIterator> FirstFitStrategy::findRange(std::vector<ChunkAllocation>& _allocations,
+                                                                       vk::DeviceSize size)
+{
+    auto foundChunk = std::find_if(_allocations.begin(), _allocations.end(),
+                                   [&](ChunkAllocation& alloc) { return alloc.maxFreeRange->size > size; });
 
-std::pair<ChunkIterator, LocationIterator>
-FirstFitStrategy::findRange(std::vector<ChunkAllocation> &_allocations, vk::DeviceSize size) {
-
-    auto foundChunk =std::find_if(_allocations.begin(), _allocations.end(),
-                                  [&](ChunkAllocation& alloc){ return alloc.maxFreeRange->size > size;});
-
-    if (foundChunk == _allocations.end()) {
+    if (foundChunk == _allocations.end())
+    {
         return std::make_pair(_allocations.end(), LocationIterator());
     }
 
     auto& chunk = *foundChunk;
 
     auto foundRange = std::find_if(chunk.freeList.begin(), chunk.freeList.end(),
-                                   [&](MemoryLocation& loc) {return loc.size >= size;});
+                                   [&](MemoryLocation& loc) { return loc.size >= size; });
 
     SAIGA_ASSERT(foundRange != chunk.freeList.end(), "free size is invalid.");
 
     return std::make_pair(foundChunk, foundRange);
 }
 
-}
-}
-}
+}  // namespace Memory
+}  // namespace Vulkan
+}  // namespace Saiga
--- src/saiga/vulkan/memory/FitStrategy.h
+++ src/saiga/vulkan/memory/FitStrategy.h
@@ -3,24 +3,27 @@
 //
 
 #pragma once
-#include "saiga/export.h"
-#include "ChunkAllocation.h"
 #include <tuple>
-namespace Saiga{
-namespace Vulkan{
-namespace Memory{
-
-struct SAIGA_GLOBAL FitStrategy {
-        virtual std::pair<ChunkIterator, LocationIterator> findRange(std::vector<ChunkAllocation> & _allocations, vk::DeviceSize size) = 0;
+#include "ChunkAllocation.h"
+#include "saiga/export.h"
+namespace Saiga
+{
+namespace Vulkan
+{
+namespace Memory
+{
+struct SAIGA_GLOBAL FitStrategy
+{
+    virtual std::pair<ChunkIterator, LocationIterator> findRange(std::vector<ChunkAllocation>& _allocations,
+                                                                 vk::DeviceSize size) = 0;
 };
 
-struct SAIGA_GLOBAL FirstFitStrategy : public FitStrategy{
-    std::pair<ChunkIterator, LocationIterator>
-    findRange(std::vector<ChunkAllocation> &_allocations, vk::DeviceSize size) override;
+struct SAIGA_GLOBAL FirstFitStrategy : public FitStrategy
+{
+    std::pair<ChunkIterator, LocationIterator> findRange(std::vector<ChunkAllocation>& _allocations,
+                                                         vk::DeviceSize size) override;
 };
 
-}
-}
-}
-
-
+}  // namespace Memory
+}  // namespace Vulkan
+}  // namespace Saiga
