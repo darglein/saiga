b4f11b4686fbaa3c1a9f373d4c7f0455bc957b16
Darius Rueckert <darius.rueckert@fau.de>
2018-12-04 14:50:33 +0100

    object buffer

--- src/saiga/vulkan/buffer/ObjectBuffer.h
+++ src/saiga/vulkan/buffer/ObjectBuffer.h
@@ -21,22 +21,51 @@ template <typename Obj>
 class SAIGA_GLOBAL ObjectBuffer : public Buffer
 {
     using pointer         = Obj*;
+    using const_pointer   = const Obj*;
     using reference       = Obj&;
     using const_reference = const Obj&;
 
    public:
+    ~ObjectBuffer()
+    {
+        // Note: Buffer::destroy will be called by the destructor of Buffer
+        if (m_memoryLocation) destruct();
+    }
+
     void init(VulkanBase& base, const vk::BufferUsageFlags& usage)
     {
         createBuffer(base, sizeof(Obj), usage,
                      vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent);
+        // Construct the object in the buffer
+        new (get()) Obj();
     }
 
+    void destroy()
+    {
+        if (m_memoryLocation) destruct();
+        Buffer::destroy();
+    }
 
-    pointer operator->() { return static_cast<pointer>(m_memoryLocation.mappedPointer); }
+    pointer get() { return static_cast<pointer>(m_memoryLocation.mappedPointer); }
+    const_pointer get() const { return static_cast<const_pointer>(m_memoryLocation.mappedPointer); }
 
-    reference operator*() { return *static_cast<pointer>(m_memoryLocation.mappedPointer); }
+    pointer operator->() { return get(); }
+    reference operator*() { return *get(); }
+    const_reference operator*() const { return *get(); }
 
-    const_reference operator*() const { return *static_cast<pointer>(m_memoryLocation.mappedPointer); }
+   private:
+    void destruct() { get()->~Obj(); }
 };
+
+/**
+ * A spezialization of the above, because this is the most common use-case.
+ */
+template <typename Obj>
+class SAIGA_GLOBAL CoherentUniformObject : public ObjectBuffer<Obj>
+{
+   public:
+    void init(VulkanBase& base) { ObjectBuffer<Obj>::init(base, vk::BufferUsageFlagBits::eUniformBuffer); }
+};
+
 }  // namespace Vulkan
 }  // namespace Saiga
