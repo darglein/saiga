0dc03998698f76acfdf1d3d720460b2bbd9eaac3
Peter Eichinger <peter.eichinger@gmail.com>
2019-01-21 16:12:24 +0100

    Defragmenter is (re)starting asynchronously after start() and pausing when stop() is called

--- src/saiga/vulkan/memory/Defragmenter.h
+++ src/saiga/vulkan/memory/Defragmenter.h
@@ -15,34 +15,51 @@
 class Defragmenter
 {
    private:
-    bool start_defrag, running;
-    std::mutex find_mutex;
-    std::condition_variable find_condition;
-
-   public:
-    Defragmenter()
-    {
-        std::thread worker(&Defragmenter::worker_func, this);
-        {
-            std::lock_guard<std::mutex> lock(find_mutex);
-            start_defrag = true;
-        }
-        find_condition.notify_one();
-    }
-
-    void pause() { running = false; }
+    bool running, quit;
+    std::mutex run_mutex;
+    std::condition_variable run_cond;
+    std::thread worker;
 
     void worker_func()
     {
-        std::unique_lock<std::mutex> lock(find_mutex);
-        find_condition.wait(lock, [this] { return start_defrag; });
-
-        // while (start_defrag)
-        //{
-        while (running)
+        while (!quit)
         {
-            LOG(INFO) << "Running";
+            std::unique_lock<std::mutex> lock(run_mutex);
+            run_cond.wait(lock, [this] { return running || quit; });
+
+            while (running)
+            {
+                LOG(INFO) << "Running";
+            }
         }
-        //}
     }
+
+   public:
+    Defragmenter() : running(false), quit(false), worker(&Defragmenter::worker_func, this) {}
+
+    ~Defragmenter()
+    {
+        quit    = true;
+        running = false;
+
+        {
+            std::lock_guard<std::mutex> lock(run_mutex);
+        }
+        run_cond.notify_one();
+        worker.join();
+    }
+
+    void start()
+    {
+        if (running)
+        {
+            return;
+        }
+        {
+            std::lock_guard<std::mutex> lock(run_mutex);
+            running = true;
+        }
+        run_cond.notify_one();
+    }
+    void stop() { running = false; }
 };
