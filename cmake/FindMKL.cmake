5e85a85f19f316db34ecb7040c76a066fb3a1cac
Darius Rueckert <darius.rueckert@fau.de>
2019-02-05 16:54:59 +0100

    fix mkl cmake again

--- cmake/FindMKL.cmake
+++ cmake/FindMKL.cmake
@@ -1,31 +1,38 @@
 
+# This can be specified by the user to point to the install dir
+# For windows this might be C:/Program Files (x86)/IntelSWTools/compilers_and_libraries_2019.2.190/windows/mkl
+set(MKL_DIR "/usr/" CACHE PATH "The directory where mkl was installed.")
 
-set(MKLLIB "C:/Program Files (x86)/IntelSWTools/compilers_and_libraries_2019.2.190/windows/mkl")
 
-    find_path(MKL_INCLUDE_DIR
-        mkl.h
-        PATHS
-            /usr/local
-			${MKLLIB}/include
+find_path(MKL_INCLUDE_DIR
+    mkl.h
+    PATHS
+    /usr/local
+    ${MKL_DIR}/include
+    PATH_SUFFIXES
+    include/mkl
+    include
+    mkl
     )
-	
+
+
 find_library(MKL_LIBRARIES_CORE
-  mkl_core
-  PATHS
-  ${MKLLIB}/lib/intel64
-)
+    mkl_core
+    PATHS
+    ${MKL_DIR}/lib/intel64
+    )
 
 find_library(MKL_LIBRARIES_LP64
-  mkl_intel_lp64
-  PATHS
-  ${MKLLIB}/lib/intel64
-)
+    mkl_intel_lp64
+    PATHS
+    ${MKL_DIR}/lib/intel64
+    )
 
 find_library(MKL_LIBRARIES_SEQUENTIAL
-  mkl_sequential
-  PATHS
-  ${MKLLIB}/lib/intel64
-)
+    mkl_sequential
+    PATHS
+    ${MKL_DIR}/lib/intel64
+    )
 
 set(MKL_LIBRARIES ${MKL_LIBRARIES_CORE} ${MKL_LIBRARIES_LP64} ${MKL_LIBRARIES_SEQUENTIAL})
 
