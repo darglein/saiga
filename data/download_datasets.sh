a94c289df11de9723b27aed55908cfc6b00bb63b
Darius Rueckert <darius.rueckert@fau.de>
2019-02-28 14:07:47 +0100

    new datasets

--- data/download_datasets.sh
+++ data/download_datasets.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+
+# vision scenes
+wget http://cloud9.cs.fau.de/index.php/s/0fQkKJzGHyVxSGf/download -O vision/data.zip
+cd vision
+unzip data.zip
+rm data.zip
+cd ..
+
 # vision bal datasets
 wget http://cloud9.cs.fau.de/index.php/s/CIwSqA3RuaDvz38/download -O vision/bal/data.zip
 cd vision/bal
 unzip data.zip
 rm data.zip
 cd ../..
+
+
