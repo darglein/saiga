e34365836df0cce7452754c486e75ac91768e9eb
Darius Rueckert <darius.rueckert@fau.de>
2019-04-24 10:48:47 +0200

    cmake stuff

---
+++ samples/core/filesystem/filesystem_main.cpp
@@ -0,0 +1,25 @@
+﻿/**
+ * Copyright (c) 2017 Darius Rückert
+ * Licensed under the MIT License.
+ * See LICENSE file for more information.
+ */
+
+#include "saiga/config.h"
+#include "saiga/core/Core.h"
+
+#include <filesystem>
+#include <iostream>
+
+namespace fs = std::filesystem;
+using namespace std;
+
+int main()
+{
+    fs::path aPath{"./path/to/file.txt"};
+
+    cout << "Parent path: " << aPath.parent_path() << endl;
+    cout << "Filename: " << aPath.filename() << endl;
+    cout << "Extension: " << aPath.extension() << endl;
+
+    return 0;
+}
