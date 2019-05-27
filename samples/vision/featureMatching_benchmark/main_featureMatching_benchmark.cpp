1e07f0ef5ad9eaec3980ed4b0bef29255b5bea13
Darius Rueckert <darius.rueckert@fau.de>
2019-05-27 11:50:55 +0200

    added matching sample

---
+++ samples/vision/featureMatching_benchmark/main_featureMatching_benchmark.cpp
@@ -0,0 +1,23 @@
+﻿/**
+ * Copyright (c) 2017 Darius Rückert
+ * Licensed under the MIT License.
+ * See LICENSE file for more information.
+ */
+#include "saiga/core/framework/framework.h"
+#include "saiga/core/util/fileChecker.h"
+#include "saiga/core/util/random.h"
+#include "saiga/vision/Eigen_Compile_Checker.h"
+#include "saiga/vision/VisionIncludes.h"
+
+
+int main(int, char**)
+{
+    Saiga::SaigaParameters saigaParameters;
+    Saiga::initSample(saigaParameters);
+    Saiga::initSaiga(saigaParameters);
+
+    Saiga::EigenHelper::checkEigenCompabitilty<15357>();
+    Saiga::Random::setSeed(93865023985);
+
+    return 0;
+}
