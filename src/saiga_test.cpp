d18c0e1307d803cf75cf10c976efaf10f3f9862e
Darius Rueckert <darius.rueckert@fau.de>
2019-09-18 17:49:09 +0200

    added debian build option

---
+++ src/saiga_test.cpp
@@ -0,0 +1,22 @@
+﻿/**
+ * Copyright (c) 2017 Darius Rückert
+ * Licensed under the MIT License.
+ * See LICENSE file for more information.
+ */
+
+#include "saiga/config.h"
+#include "saiga/core/Core.h"
+#include "saiga/core/math/Eigen_Compile_Checker.h"
+
+using namespace Saiga;
+
+int main()
+{
+    initSaigaSampleNoWindow();
+
+
+    auto saigaFlags = EigenHelper::getSaigaEigenCompileFlags();
+    std::cout << saigaFlags << std::endl;
+
+    return 0;
+}
