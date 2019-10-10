ae72c40e68131eac3222c98df6be1a52d83f3471
Darius Rueckert <darius.rueckert@fau.de>
2019-10-10 16:01:37 +0200

    random vec4 + interop in header

--- src/saiga/cuda/interop.h
+++ src/saiga/cuda/interop.h
@@ -7,11 +7,13 @@
 #pragma once
 
 #include "saiga/cuda/cudaHelper.h"
+#include "saiga/cuda/interop.h"
 #include "saiga/opengl/opengl.h"
 
 #include <iostream>
 
-struct cudaGraphicsResource;
+#include <cuda_gl_interop.h>
+
 
 namespace Saiga
 {
@@ -19,38 +21,76 @@ namespace CUDA
 {
 class SAIGA_CUDA_API Interop
 {
-   private:
-    unsigned int gl_buffer;
-    cudaGraphicsResource* graphic_resource;
-    void* device_ptr;
-    size_t size;
-    bool mapped;
-
-
-    GLenum gl_target;
-
    public:
-    Interop();
-    ~Interop();
+    ~Interop() { unregisterBuffer(); }
 
     // Registers an OpenGL buffer object. Wrapper for cudaGraphicsGLRegisterBuffer
-    void registerBuffer(int glbuffer);
+    void registerBuffer(int glbuffer)
+    {
+        this->gl_buffer = glbuffer;
+        CHECK_CUDA_ERROR(cudaGraphicsGLRegisterBuffer(&graphic_resource, glbuffer, cudaGraphicsRegisterFlagsNone));
+    }
 
-    void unregisterBuffer();
+    void unregisterBuffer()
+    {
+        if (mapped) unmap();
+        if (graphic_resource) CHECK_CUDA_ERROR(cudaGraphicsUnregisterResource(graphic_resource));
+    }
 
     // Maps graphic resource to be accessed by CUDA. Wrapper for cudaGraphicsMapResources
-    void map();
+    void map()
+    {
+        // map OpenGL buffer object for writing from CUDA
+        CHECK_CUDA_ERROR(cudaGraphicsMapResources(1, &graphic_resource, 0));
+
+        mapped = true;
+    }
 
     // Unmap graphics resources. Wrapper for cudaGraphicsUnmapResources
-    void unmap();
+    void unmap()
+    {
+        // unmap buffer object
+        CHECK_CUDA_ERROR(cudaGraphicsUnmapResources(1, &graphic_resource, 0));
+        mapped = false;
+    }
 
     // Get an device pointer through which to access a mapped graphics resource. Wrapper for
     // cudaGraphicsResourceGetMappedPointer
-    void* getDevicePtr();
+    void* getDevicePtr()
+    {
+        CHECK_CUDA_ERROR(cudaGraphicsResourceGetMappedPointer(&device_ptr, &size, graphic_resource));
+        return device_ptr;
+    }
 
     size_t get_size() { return size; }
-    void initImage(unsigned int gl_buffer, GLenum gl_target);
-    void mapImage();
+    void initImage(unsigned int gl_buffer, GLenum gl_target)
+    {
+        this->gl_buffer = gl_buffer;
+        this->gl_target = gl_target;
+        CHECK_CUDA_ERROR(cudaGraphicsGLRegisterImage(&graphic_resource, gl_buffer, gl_target,
+                                                     cudaGraphicsRegisterFlagsSurfaceLoadStore));
+    }
+    void mapImage()
+    {
+        // map OpenGL buffer object for writing from CUDA
+        cudaArray* array;  // = (cudaArray *)device_ptr;
+        CHECK_CUDA_ERROR(cudaGraphicsMapResources(1, &graphic_resource, 0));
+        CHECK_CUDA_ERROR(cudaGraphicsSubResourceGetMappedArray(&array, graphic_resource, 0, 0));
+
+        //    std::cout<<array<<std::endl;
+        device_ptr = array;
+        mapped     = true;
+    }
+
+   private:
+    unsigned int gl_buffer;
+    cudaGraphicsResource* graphic_resource = nullptr;
+    void* device_ptr;
+    size_t size;
+    bool mapped = false;
+
+
+    GLenum gl_target;
 };
 
 }  // namespace CUDA
