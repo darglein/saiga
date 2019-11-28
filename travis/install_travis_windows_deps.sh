0c61a1a8cd7af43cb52724cfe92137218662c7a1
Darius Rueckert <darius.rueckert@fau.de>
2019-11-28 17:02:02 +0100

    another windows travis test

--- travis/install_travis_windows_deps.sh
+++ travis/install_travis_windows_deps.sh
@@ -1,2 +1,9 @@
+mkdir "C:/Users/travis/install"
 
-git clone https://github.com/eigenteam/eigen-git-mirror.git /tmp/eigen
+git clone https://github.com/eigenteam/eigen-git-mirror.git "C:/Users/travis/eigen"
+cd "C:/Users/travis/eigen"
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX="C:/Users/travis/install" ..
+cmake --build . -j4
+cmake --install .
