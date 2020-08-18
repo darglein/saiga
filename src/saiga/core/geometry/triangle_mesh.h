40730327889a8368fa67c3a1c202ea395481e2ec
Darius Rueckert <darius.rueckert@fau.de>
2020-08-18 16:39:32 +0200

    faster off write

--- src/saiga/core/geometry/triangle_mesh.h
+++ src/saiga/core/geometry/triangle_mesh.h
@@ -516,21 +516,24 @@ void TriangleMesh<vertex_t, index_t>::removeDegenerateFaces()
 template <typename vertex_t, typename index_t>
 void TriangleMesh<vertex_t, index_t>::saveMeshOff(std::ostream& strm) const
 {
-    strm << "OFF" << std::endl;
+    strm << "OFF"
+         << "\n";
     // first line: number of vertices, number of faces, number of edges (can be ignored)
-    strm << vertices.size() << " " << faces.size() << " 0" << std::endl;
+    strm << vertices.size() << " " << faces.size() << " 0"
+         << "\n";
 
     for (auto const& v : vertices)
     {
-        strm << v.position[0] << " " << v.position[1] << " " << v.position[2] << std::endl;
+        strm << v.position[0] << " " << v.position[1] << " " << v.position[2] << "\n";
     }
 
     for (auto const& f : faces)
     {
         strm << "3"
-             << " " << f[0] << " " << f[1] << " " << f[2] << std::endl;
+             << " " << f[0] << " " << f[1] << " " << f[2] << "\n";
     }
 }
+
 
 template <typename vertex_t, typename index_t>
 std::ostream& operator<<(std::ostream& os, const TriangleMesh<vertex_t, index_t>& dt)
