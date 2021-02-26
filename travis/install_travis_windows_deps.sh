182189b43f4a6bdfbc41eeeb7926dc607330827d
Darius Rueckert <darius.rueckert@fau.de>
2021-02-26 18:40:23 +0100

    travis vcpk test

--- travis/install_travis_windows_deps.sh
+++ travis/install_travis_windows_deps.sh
@@ -1,3 +1,10 @@
+export VCPKG_DEFAULT_TRIPLET=x86-windows
+git clone https://github.com/Microsoft/vcpkg.git
+cd vcpkg
+./bootstrap-vcpkg.bat
+./vcpkg integrate install
+./vcpkg install sdl2
+
 SRC_DIR="C:/Users/travis/eigen"
 INSTALL_DIR="C:/Users/travis/install"
 
