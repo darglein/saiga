1954e82e2389f55b834fc2cedacaf97c86e53ba1
Darius Rueckert <darius.rueckert@fau.de>
2022-05-10 17:08:38 +0200

    update cuda timer system

--- src/saiga/cuda/imgui_cuda.h
+++ src/saiga/cuda/imgui_cuda.h
@@ -16,7 +16,7 @@ namespace CUDA
 class CudaTimerSystem : public TimerSystem
 {
    public:
-    CudaTimerSystem() : TimerSystem("CUDA Timer") {}
+    CudaTimerSystem(cudaStream_t stream = 0) : TimerSystem("CUDA Timer"), stream(stream) {}
 
    protected:
     virtual void BeginFrameImpl() override { base_timer.startTimer(); }
@@ -24,12 +24,13 @@ class CudaTimerSystem : public TimerSystem
 
     virtual std::unique_ptr<TimestampTimer> CreateTimer() override
     {
-        auto timer        = std::make_unique<CUDA::RelativeCudaTimer>();
+        auto timer        = std::make_unique<CUDA::RelativeCudaTimer>(stream);
         timer->base_timer = &base_timer;
         return timer;
     }
 
     CUDA::MultiFrameTimer base_timer;
+    cudaStream_t stream = 0;
 };
 }  // namespace CUDA
 }  // namespace Saiga
