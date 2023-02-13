02cecbd6d88e830b5f33bb1c0f207e25aea0fb90
Linus <linus.franke@fau.de>
2023-02-13 14:57:28 +0100

    cleanup

--- tests/numeric_derivative.h
+++ tests/numeric_derivative.h
@@ -1,18 +1,20 @@
 ﻿/**
  * Copyright (c) 2021 Darius Rückert
  * Licensed under the MIT License.
- * See LICENSE file for more information.true
+ * See LICENSE file for more information.
  */
 
 
 #include "saiga/core/math/all.h"
 
 
-namespace Saiga{
+namespace Saiga
+{
 
 template <typename T, int num_params, int num_residuals, typename Functor>
 Eigen::Matrix<T, num_residuals, 1> EvaluateNumeric(Functor f, const Eigen::Matrix<T, num_params, 1>& params,
-                                                   Matrix<double, num_residuals, num_params>* jacobian = nullptr, double eps = 1e-4)
+                                                   Matrix<double, num_residuals, num_params>* jacobian = nullptr,
+                                                   double eps                                          = 1e-4)
 {
     Eigen::Matrix<T, num_residuals, 1> residual = f(params);
 
@@ -37,4 +39,4 @@ Eigen::Matrix<T, num_residuals, 1> EvaluateNumeric(Functor f, const Eigen::Matri
     return residual;
 }
 
-}
+}  // namespace Saiga
