26137da059b685f5cbbfef16e65c991d77f4b86a
Darius Rueckert <darius.rueckert@fau.de>
2024-11-29 06:59:28 +0100

    add vec5

--- src/saiga/vision/torch/EigenTensor.h
+++ src/saiga/vision/torch/EigenTensor.h
@@ -58,8 +58,17 @@ inline Saiga::Vec2* at::TensorBase::data_ptr<Saiga::Vec2>() const
 // tensor.data_ptr<vec3>() on tensors with the shape
 //   [x,x,...,x,3]
 template <>
+inline Saiga::vec5* at::Tensor::data_ptr<Saiga::vec5>() const
+{
+    if (!defined()) return nullptr;
+    CHECK_EQ(size(dim() - 1), 5);
+    CHECK_EQ(stride(dim() - 1), 1);
+    return (Saiga::vec5*)data_ptr<float>();
+}
+template <>
 inline Saiga::vec4* at::Tensor::data_ptr<Saiga::vec4>() const
 {
+    if (!defined()) return nullptr;
     CHECK_EQ(size(dim() - 1), 4);
     CHECK_EQ(stride(dim() - 1), 1);
     return (Saiga::vec4*)data_ptr<float>();
@@ -67,6 +76,7 @@ inline Saiga::vec4* at::Tensor::data_ptr<Saiga::vec4>() const
 template <>
 inline Saiga::vec3* at::Tensor::data_ptr<Saiga::vec3>() const
 {
+    if (!defined()) return nullptr;
     CHECK_EQ(size(dim() - 1), 3);
     CHECK_EQ(stride(dim() - 1), 1);
     return (Saiga::vec3*)data_ptr<float>();
@@ -74,6 +84,7 @@ inline Saiga::vec3* at::Tensor::data_ptr<Saiga::vec3>() const
 template <>
 inline Saiga::vec2* at::Tensor::data_ptr<Saiga::vec2>() const
 {
+    if (!defined()) return nullptr;
     CHECK_EQ(size(dim() - 1), 2);
     CHECK_EQ(stride(dim() - 1), 1);
     return (Saiga::vec2*)data_ptr<float>();
@@ -81,6 +92,7 @@ inline Saiga::vec2* at::Tensor::data_ptr<Saiga::vec2>() const
 template <>
 inline Saiga::ivec2* at::Tensor::data_ptr<Saiga::ivec2>() const
 {
+    if (!defined()) return nullptr;
     CHECK_EQ(size(dim() - 1), 2);
     CHECK_EQ(stride(dim() - 1), 1);
     return (Saiga::ivec2*)data_ptr<int>();
@@ -88,13 +100,23 @@ inline Saiga::ivec2* at::Tensor::data_ptr<Saiga::ivec2>() const
 template <>
 inline Saiga::Quat* at::Tensor::data_ptr<Saiga::Quat>() const
 {
+    if (!defined()) return nullptr;
     CHECK_EQ(size(dim() - 1), 4);
     CHECK_EQ(stride(dim() - 1), 1);
     return (Saiga::Quat*)data_ptr<double>();
 }
 template <>
+inline Saiga::Vec5* at::Tensor::data_ptr<Saiga::Vec5>() const
+{
+    if (!defined()) return nullptr;
+    CHECK_EQ(size(dim() - 1), 5);
+    CHECK_EQ(stride(dim() - 1), 1);
+    return (Saiga::Vec5*)data_ptr<double>();
+}
+template <>
 inline Saiga::Vec4* at::Tensor::data_ptr<Saiga::Vec4>() const
 {
+    if (!defined()) return nullptr;
     CHECK_EQ(size(dim() - 1), 4);
     CHECK_EQ(stride(dim() - 1), 1);
     return (Saiga::Vec4*)data_ptr<double>();
@@ -102,6 +124,7 @@ inline Saiga::Vec4* at::Tensor::data_ptr<Saiga::Vec4>() const
 template <>
 inline Saiga::Vec3* at::Tensor::data_ptr<Saiga::Vec3>() const
 {
+    if (!defined()) return nullptr;
     CHECK_EQ(size(dim() - 1), 3);
     CHECK_EQ(stride(dim() - 1), 1);
     return (Saiga::Vec3*)data_ptr<double>();
@@ -109,6 +132,7 @@ inline Saiga::Vec3* at::Tensor::data_ptr<Saiga::Vec3>() const
 template <>
 inline Saiga::Vec2* at::Tensor::data_ptr<Saiga::Vec2>() const
 {
+    if (!defined()) return nullptr;
     CHECK_EQ(size(dim() - 1), 2);
     CHECK_EQ(stride(dim() - 1), 1);
     return (Saiga::Vec2*)data_ptr<double>();
